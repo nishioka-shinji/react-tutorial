962d84a232cd84e8ba5dec6ed9ae4613690a7acd
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-06-24 08:34:37 +0900

    Writing Markup with JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Gallery from './Gallery.js';
-
-export default function App() {
+export default function Bio() {
   return (
-    <div>
-      <Gallery />
-    </div>
+    <>
+      <div className="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p className="summary">
+        You can find my thoughts here.
+        <br/>
+        <br/>
+        <b>And <i>pictures</i></b> of scientists!
+      </p>
+    </>
   );
 }
