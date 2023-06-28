9515bbee2151f6595018aace44e84bb18a628c40
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-06-28 22:43:49 +0900

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-function Card({ children, title }) {
+const drinks = {
+  tea: {
+    part: 'leaf',
+    caffeine: '15–70 mg/cup',
+    age: '4,000+ years'
+  },
+  coffee: {
+    part: 'bean',
+    caffeine: '80–185 mg/cup',
+    age: '1,000+ years'
+  }
+};
+
+function Drink({ name }) {
+  const info = drinks[name];
   return (
-    <div className="card">
-      <div className="card-content">
-        <h1>{title}</h1>
-        {children}
-      </div>
-    </div>
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{info.part}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{info.caffeine}</dd>
+        <dt>Age</dt>
+        <dd>{info.age}</dd>
+      </dl>
+    </section>
   );
 }
 
-export default function Profile() {
+export default function DrinkList() {
   return (
     <div>
-      <Card title="photo">
-        <img
-          className="avatar"
-          src="https://i.imgur.com/OKS67lhm.jpg"
-          alt="Aklilu Lemma"
-          width={70}
-          height={70}
-        />
-      </Card>
-      <Card title="about">
-        <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-      </Card>
+      <Drink name="tea" />
+      <Drink name="coffee" />
     </div>
   );
 }
