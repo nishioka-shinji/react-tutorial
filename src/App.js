dec148b733ed6c2c4c49404bbd5c27f25401f432
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-07-01 06:51:27 +0900

    Rendering Lists

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-const drinks = {
-  tea: {
-    part: 'leaf',
-    caffeine: '15–70 mg/cup',
-    age: '4,000+ years'
-  },
-  coffee: {
-    part: 'bean',
-    caffeine: '80–185 mg/cup',
-    age: '1,000+ years'
-  }
+import { Fragment } from 'react';
+
+const poem = {
+  lines: [
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.'
+  ]
 };
 
-function Drink({ name }) {
-  const info = drinks[name];
+export default function Poem() {
   return (
-    <section>
-      <h1>{name}</h1>
-      <dl>
-        <dt>Part of plant</dt>
-        <dd>{info.part}</dd>
-        <dt>Caffeine content</dt>
-        <dd>{info.caffeine}</dd>
-        <dt>Age</dt>
-        <dd>{info.age}</dd>
-      </dl>
-    </section>
-  );
-}
-
-export default function DrinkList() {
-  return (
-    <div>
-      <Drink name="tea" />
-      <Drink name="coffee" />
-    </div>
+    <article>
+      {poem.lines.map((line, i) =>
+        <Fragment key={i}>
+          {i > 0 && <hr />}
+          <p>{line}</p>
+        </Fragment>
+      )}
+    </article>
   );
 }
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,7 @@
-export function getImageUrl(person, size) {
+export function getImageUrl(person) {
   return (
     'https://i.imgur.com/' +
     person.imageId +
-    size +
-    '.jpg'
+    's.jpg'
   );
 }
