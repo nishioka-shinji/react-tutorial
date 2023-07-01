5613a3515f1a370015061cc2df2891fa96769521
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-07-01 18:25:38 +0900

    Render and Commit

--- src/Image.js
+++ src/Image.js
@@ -1,15 +1,4 @@
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Inspiring Sculptures</h1>
-      <Image />
-      <Image />
-      <Image />
-    </section>
-  );
-}
-
-function Image() {
+export default function Image() {
   return (
     <img
       src="https://i.imgur.com/ZF6s192.jpg"
