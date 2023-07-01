40725640d2d1cf566c321ce8e681ed3bb5efaccf
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-07-01 18:52:11 +0900

    tate as a Snapshot

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { useState } from 'react';
 
-export default function FeedbackForm() {
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
+
   function handleClick() {
-    const name = prompt('What is your name?');
-    alert(`Hello, ${name}!`);
+    setWalk(!walk);
+    alert(walk ? 'Stop is next' : 'Walk is next');
   }
 
   return (
-    <button onClick={handleClick}>
-      Greet
-    </button>
+    <>
+      <button onClick={handleClick}>
+        Change to {walk ? 'Stop' : 'Walk'}
+      </button>
+      <h1 style={{
+        color: walk ? 'darkgreen' : 'darkred'
+      }}>
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
+    </>
   );
 }
