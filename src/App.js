68091600e34061a2da9f43b57a9d2009381e9639
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-07-01 20:11:43 +0900

    Queueing a Series of State Updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import { useState } from 'react';
+import { getFinalState } from './processQueue.js';
 
-export default function TrafficLight() {
-  const [walk, setWalk] = useState(true);
+function increment(n) {
+  return n + 1;
+}
+increment.toString = () => 'n => n+1';
 
-  function handleClick() {
-    setWalk(!walk);
-    alert(walk ? 'Stop is next' : 'Walk is next');
-  }
-
+export default function App() {
   return (
     <>
-      <button onClick={handleClick}>
-        Change to {walk ? 'Stop' : 'Walk'}
-      </button>
-      <h1 style={{
-        color: walk ? 'darkgreen' : 'darkred'
+      <TestCase
+        baseState={0}
+        queue={[1, 1, 1]}
+        expected={1}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          increment,
+          increment,
+          increment
+        ]}
+        expected={3}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          5,
+          increment,
+        ]}
+        expected={6}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          5,
+          increment,
+          42,
+        ]}
+        expected={42}
+      />
+    </>
+  );
+}
+
+function TestCase({
+  baseState,
+  queue,
+  expected
+}) {
+  const actual = getFinalState(baseState, queue);
+  return (
+    <>
+      <p>Base state: <b>{baseState}</b></p>
+      <p>Queue: <b>[{queue.join(', ')}]</b></p>
+      <p>Expected result: <b>{expected}</b></p>
+      <p style={{
+        color: actual === expected ?
+          'green' :
+          'red'
       }}>
-        {walk ? 'Walk' : 'Stop'}
-      </h1>
+        Your result: <b>{actual}</b>
+        {' '}
+        ({actual === expected ?
+          'correct' :
+          'wrong'
+        })
+      </p>
     </>
   );
 }
