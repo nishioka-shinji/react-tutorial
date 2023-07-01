5abfd3f24baffebf188bb5300f955712d68100e4
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-07-01 18:07:41 +0900

    State: A Component's Memory

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import { useState } from 'react';
-import ColorSwitch from './ColorSwitch.js';
 
-export default function App() {
-  const [clicks, setClicks] = useState(0);
-
-  function handleClickOutside() {
-    setClicks(c => c + 1);
-  }
-
-  function getRandomLightColor() {
-    let r = 150 + Math.round(100 * Math.random());
-    let g = 150 + Math.round(100 * Math.random());
-    let b = 150 + Math.round(100 * Math.random());
-    return `rgb(${r}, ${g}, ${b})`;
-  }
-
-  function handleChangeColor() {
-    let bodyStyle = document.body.style;
-    bodyStyle.backgroundColor = getRandomLightColor();
+export default function FeedbackForm() {
+  function handleClick() {
+    const name = prompt('What is your name?');
+    alert(`Hello, ${name}!`);
   }
 
   return (
-    <div style={{ width: '100%', height: '100%' }} onClick={handleClickOutside}>
-      <ColorSwitch onChangeColor={handleChangeColor} />
-      <br />
-      <br />
-      <h2>Clicks on the page: {clicks}</h2>
-    </div>
+    <button onClick={handleClick}>
+      Greet
+    </button>
   );
 }
