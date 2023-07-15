53ece1b86e692b6081aa09ec7652f60235c7fc36
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-07-16 07:10:25 +0900

    Reacting to Input with State

--- src/App.js
+++ src/App.js
@@ -1,59 +1,45 @@
 import { useState } from 'react';
-import { useImmer } from 'use-immer';
-import AddTodo from './AddTodo.js';
-import TaskList from './TaskList.js';
 
-let nextId = 3;
-const initialTodos = [
-  { id: 0, title: 'Buy milk', done: true },
-  { id: 1, title: 'Eat tacos', done: false },
-  { id: 2, title: 'Brew tea', done: false },
-];
-
-export default function TaskApp() {
-  const [todos, updateTodos] = useImmer(
-    initialTodos
-  );
-
-  function handleAddTodo(title) {
-    updateTodos(draft => {
-      draft.push({
-        id: nextId++,
-        title: title,
-        done: false
-      });
-    });
-  }
-
-  function handleChangeTodo(nextTodo) {
-    updateTodos(draft => {
-      const todo = draft.find(t =>
-        t.id === nextTodo.id
-      );
-      todo.title = nextTodo.title;
-      todo.done = nextTodo.done;
-    });
-  }
-
-  function handleDeleteTodo(todoId) {
-    updateTodos(draft => {
-      const index = draft.findIndex(t =>
-        t.id === todoId
-      );
-      draft.splice(index, 1);
-    });
-  }
+export default function EditProfile() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [firstName, setFirstName] = useState('Jane');
+  const [lastName, setLastName] = useState('Jacobs');
 
   return (
-    <>
-      <AddTodo
-        onAddTodo={handleAddTodo}
-      />
-      <TaskList
-        todos={todos}
-        onChangeTodo={handleChangeTodo}
-        onDeleteTodo={handleDeleteTodo}
-      />
-    </>
+    <form onSubmit={e => {
+      e.preventDefault();
+      setIsEditing(!isEditing);
+    }}>
+      <label>
+        First name:{' '}
+        {isEditing ? (
+          <input
+            value={firstName}
+            onChange={e => {
+              setFirstName(e.target.value)
+            }}
+          />
+        ) : (
+          <b>{firstName}</b>
+        )}
+      </label>
+      <label>
+        Last name:{' '}
+        {isEditing ? (
+          <input
+            value={lastName}
+            onChange={e => {
+              setLastName(e.target.value)
+            }}
+          />
+        ) : (
+          <b>{lastName}</b>
+        )}
+      </label>
+      <button type="submit">
+        {isEditing ? 'Save' : 'Edit'} Profile
+      </button>
+      <p><i>Hello, {firstName} {lastName}!</i></p>
+    </form>
   );
 }
