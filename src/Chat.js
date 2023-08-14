be64f6f4847019ca0fab0fd432c9c138404bdf0c
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-08-15 08:29:30 +0900

    Synchronizing with Effects

--- src/Chat.js
+++ src/Chat.js
@@ -1,28 +1,11 @@
-import { useState } from 'react';
-
-export default function Chat({contact, message, dispatch}) {
-  return (
-    <section className="chat">
-      <textarea
-        value={message}
-        placeholder={'Chat to ' + contact.name}
-        onChange={(e) => {
-          dispatch({
-            type: 'edited_message',
-            message: e.target.value,
-          });
-        }}
-      />
-      <br />
-      <button
-        onClick={() => {
-          alert(`Sending "${message}" to ${contact.email}`);
-          dispatch({
-            type: 'sent_message',
-          });
-        }}>
-        Send to {contact.email}
-      </button>
-    </section>
-  );
+export function createConnection() {
+  // A real implementation would actually connect to the server
+  return {
+    connect() {
+      console.log('✅ Connecting...');
+    },
+    disconnect() {
+      console.log('❌ Disconnected.');
+    }
+  };
 }
